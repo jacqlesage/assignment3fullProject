6002c117de27a5eaa4a272e84eb6289f14cabdc9
jgoodman <jgoodman@oucs1460.otago.ac.nz>
2016-09-17 12:46:25 +1200

    need to add a drop down box for the roles in our DB so I am making a method in the WorkerRole Obj. It should be in the view somehow...anyway I can get it printing directly from the method not ideal but worry about it later.

--- src/java/controller/WorkerRoleObj.java
+++ src/java/controller/WorkerRoleObj.java
@@ -10,6 +10,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -28,11 +29,13 @@ public class WorkerRoleObj {
     
     
     
-    public String getRoleOptions() throws SQLException{
+    public HashMap getRoleOptions() throws SQLException{
         //return a hasMap which I can then use for the Role type, which
         //we can then use to link the workers role to the specific Id's that have 
         //been input into the worker_role table anyway
-         String sql = "SELECT ROLE_ID, ROLE_DESCRIPTION FROM WORKER_ROLE";
+         HashMap<Integer, String> hmap = new HashMap<Integer, String>(); 
+        
+        String sql = "SELECT ROLE_ID, ROLE_DESCRIPTION FROM WORKER_ROLE";
             
              DriverManager.registerDriver (new oracle.jdbc.driver.OracleDriver());
             
@@ -44,9 +47,10 @@ public class WorkerRoleObj {
                 ResultSet rs = stmt.executeQuery(sql);
                  
                  while(rs.next()){
-                  
-                     System.out.print(rs.getInt("ROLE_ID"));
-                     System.out.println(rs.getString("ROLE_DESCRIPTION"));
+                     
+                     hmap.put(rs.getInt("ROLE_ID"), rs.getString("ROLE_DESCRIPTION"));
+//                     System.out.print(rs.getInt("ROLE_ID"));
+//                     System.out.println(rs.getString("ROLE_DESCRIPTION"));
                  }
          
          
@@ -54,7 +58,7 @@ public class WorkerRoleObj {
             System.out.println("no customer found");
             Logger.getLogger(WorkerObj.class.getName()).log(Level.SEVERE, null, ex); 
     }
-             return "s";
+             return hmap;
      }
         
         
